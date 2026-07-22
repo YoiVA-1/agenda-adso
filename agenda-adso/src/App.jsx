99c9c19921df6686cf3cab2576811ae25e3ef84d
YoiVA-1 <yoinervelez8@gmail.com>
2026-07-22 16:29:35 -0500

    commit de reactjs

---
+++ agenda-adso/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+const fecha = new Date().toLocaleString();
+return (
+ <main>
+ <h1>Hola SENA soy Yoiner Velez Areiza</h1>
+ <h2>esta es mi primera clase de Reactjs</h2>
+  </main>)}
