4ae288600869ec27db9a524a15b329321ea3b72e
juan esteban <juanagudlo270@gmail.com>
2026-07-29 15:56:13 -0500

    se añadio el archivo ejercicio2.js y se edito app.jsx

---
+++ ejercicio2.js
@@ -0,0 +1,9 @@
+
+const aprendiz = {
+    nombreA : "Yoiner", 
+    numeroF : 3412768,
+};
+const notas = [2.3 , 1.5, 3.0]
+const promedio = (notas[0] + notas[1] + notas[2]) / 3;
+
+console.log(`======================= \n SISTEMA NOTAS SENA \n ====================== \n Aprendiz: ${aprendiz.nombreA} \n Ficha: ${aprendiz.numeroF} \n Notas: ${notas} \n ====================== \n Promedio: ${promedio.toFixed(2)} \n Estado: ${promedio >= 3 ? "Aprobado" : "No Aprobado"} `)
