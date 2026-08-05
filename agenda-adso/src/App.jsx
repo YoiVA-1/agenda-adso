1fa3a729b9b5f4588dbf8646b08d088e220ed55a
juan esteban <juanagudlo270@gmail.com>
2026-08-05 16:22:49 -0500

    componentes reutilizables y props

--- agenda-adso/src/App.jsx
+++ agenda-adso/src/App.jsx
@@ -1,7 +1,64 @@
+import "./App.css";
+import ContactoCard from "./Components/ContactoCard"; 
+
 export default function App() {
-const fecha = new Date().toLocaleString();
-return (
- <main>
- <h1>Hola SENA soy Yoiner Velez Areiza</h1>
- <h2>esta es mi primera clase de Reactjs</h2>
-  </main>)}
+  const contactos = [
+    {
+      id: 1,
+      nombre: "Carolina Pérez",
+      telefono: "300 123 4567",
+      correo: "carolina@sena.edu.co",
+      etiqueta: "Compañera",
+    },
+    {
+      id: 2,
+      nombre: "Juan Díaz",
+      telefono: "301 987 6543",
+      correo: "juan@sena.edu.co",
+      etiqueta: "Instructor",
+    },
+    {
+      id: 3,
+      nombre: "Luisa Martínez",
+      telefono: "320 555 7788",
+      correo: "luisa@sena.edu.co",
+      etiqueta: "Cliente",
+    },
+    {
+      id: 4,
+      nombre: "Yoiner Velez Areiza",
+      telefono: "320 666 3452",
+      correo: "YoiVA@sena.edu.co",
+      etiqueta: "Aprendiz",
+    },
+    {
+      id: 5,
+      nombre: "Leo Ramirez Gonzales",
+      telefono: "320 567 8658",
+      correo: "LEOR@sena.edu.co",
+      etiqueta: "Aprendiz",
+    },
+  ];
+
+  return (
+    <main className="app-container">
+      <h1 className="app-title">Agenda ADSO 📒</h1>
+
+      <p className="app-subtitle">Contactos guardados</p>
+      {contactos.map((c) => (
+        <ContactoCard
+          key={c.id}            
+          nombre={c.nombre}    
+          telefono={c.telefono} 
+          correo={c.correo}     
+          etiqueta={c.etiqueta} 
+        />
+      ))}
+
+      <p className="app-nota">
+        (Versión 0.1 - solo lectura, sin agregar ni editar todavía)
+      </p>
+    </main>
+  );
+}
+
